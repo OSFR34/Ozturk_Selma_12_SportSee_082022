ecc96af702bb0e62dd207064bf7e6abac6862824
OSFR34 <selma-ozturk@outlook.com>
2022-09-15 16:17:09 +0200

    indentations

--- Frontend/src/User.js
+++ Frontend/src/User.js
@@ -2,18 +2,18 @@ import axios from "axios"
 
 //API CALLS
 export const getUser = (userId) => {
-return axios.get(`/user/${userId}`)
+    return axios.get(`/user/${userId}`)
 }
 
 export const getUserActivity = (user_id) => {
-return axios.get(`/user/${user_id}/activity`)
+    return axios.get(`/user/${user_id}/activity`)
 
 }
 
 export const getUserSessions = (user_id) => {
-return axios.get(`/user/${user_id}/average-sessions`)
+    return axios.get(`/user/${user_id}/average-sessions`)
 }
 
 export const getUserPerformance = (user_id) => {
-return axios.get(`/user/${user_id}/performance`)
+    return axios.get(`/user/${user_id}/performance`)
 }
